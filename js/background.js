eb437e9cb30deccc8a4c032479c9a6eca1d78eca
yuhaige <yuhaige@smartpos.top>
2019-06-21 13:12:50 +0800

    fix

--- js/background.js
+++ js/background.js
@@ -1,32 +1,57 @@
 chrome.webRequest.onBeforeRequest.addListener(function (details) {
+		let url = details.url;
+		let flag = false;
+	    if (url.indexOf('ajax.googleapis.com/ajax/libs')>=0) {
+	        url = details.url.replace('ajax.googleapis.com/ajax/libs', 'libs.cdnjs.net');
+	        flag = true;
+	    } else if (url.indexOf('themes.googleusercontent.com')>=0) {
+	        url = details.url.replace('themes.googleusercontent.com', 'themes.loli.net');
+	        flag = true;
+	    } else if (url.indexOf('secure.gravatar.com')>=0) {
+	        url = details.url.replace('secure.gravatar.com', 'gravatar.loli.net');
+	        flag = true;
+	    }
+	    if (flag) {
+	    	return {redirectUrl: url};
+	    } else {
+	    	return {cancel:false};
+	    }
+	},
+	{urls: ["<all_urls>"],
+	 types: [
+        "main_frame", 
+        "sub_frame", 
+        "stylesheet", 
+        "script", 
+        "image", 
+        "object", 
+        "xmlhttprequest", 
+        "other"
+    ]},
+    ["blocking"]
+);
+chrome.webRequest.onBeforeRequest.addListener(function (details) {
         let url = details.url;
-        if (url.indexOf('ajax.googleapis.com/ajax/libs')>=0) {
-            url = details.url.replace('ajax.googleapis.com/ajax/libs', 'libs.cdnjs.net');
-            return {redirectUrl: url};
-        } else {
-            let result = 0
-            result += url.indexOf('embed.twitch.tv') + 1
-            result += url.indexOf('translate.google.com/translate_a/element.js') + 1
-            result += url.indexOf('googlesyndication.com') + 1
-            result += url.indexOf('apis.google.com/js/platform.js') + 1
-            result += url.indexOf('connect.facebook.net/en_US') + 1
-            result += url.indexOf('platform.twitter.com/widgets.js') + 1
-            result += url.indexOf('static.ads-twitter.com/uwt.js') + 1
-            result += url.indexOf('data.adsrvr.org') + 1
-            result += url.indexOf('match.adsrvr.org') + 1
-            result += url.indexOf('.google.com') + 1
-            result += url.indexOf('ib.adnxs.com') + 1
-            if (result > 0) {
-                return {cancel:true};
-            }
-        }
-        return {cancel:false};
+        let result = 0
+        result += url.indexOf('embed.twitch.tv') + 1
+        result += url.indexOf('translate.google.com/translate_a/element.js') + 1
+        result += url.indexOf('googlesyndication.com') + 1
+        result += url.indexOf('apis.google.com/js/platform.js') + 1
+        result += url.indexOf('connect.facebook.net/en_US') + 1
+        result += url.indexOf('platform.twitter.com/widgets.js') + 1
+        result += url.indexOf('static.ads-twitter.com/uwt.js') + 1
+        result += url.indexOf('data.adsrvr.org') + 1
+        result += url.indexOf('match.adsrvr.org') + 1
+        result += url.indexOf('.google.com') + 1
+        result += url.indexOf('ib.adnxs.com') + 1
+        return {cancel:result > 0};
     },
     {urls: ["https://www.mtggoldfish.com/*",
             "https://www.mtgstocks.com/*",
             "https://www.hareruyamtg.com/*",
             "https://mtgdecks.net/*",
-            "https://www.mtgtop8.com/*"]},
+            "https://www.mtgtop8.com/*",
+        	"https://gatherer.wizards.com/*"]},
     ["blocking"]
 );
     
